26ce8ecde616ff75989e7bf2dfa4ebdce579a8d5
Nicolas Schodet <nicolas.schodet@apbteam.org>
2008-04-05 11:38:02 +0200

    * digital/avr/modules/math:    - added math.h.

---
+++ digital/avr/modules/math/math.h
@@ -0,0 +1,32 @@
+#ifndef math_h
+#define math_h
+/* math.h */
+/* avr.math - Math utilities modules. {{{
+ *
+ * Copyright (C) 2008 Nicolas Schodet
+ *
+ * APBTeam:
+ *        Web: http://apbteam.org/
+ *      Email: team AT apbteam DOT org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * }}} */
+
+#define M_PI		3.14159265358979323846	/* pi */
+#define M_1_PI		0.31830988618379067154	/* 1/pi */
+#define M_SQRT2		1.41421356237309504880	/* sqrt(2) */
+
+#endif /* math_h */
