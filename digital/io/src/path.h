ec7e026ab256ccd5a8d345a902940b696b96114f
Jérémy Dufour <jeremy.dufour@apbteam.org>
2008-04-30 01:38:18 +0200

    * digital/io/src    - export the decay function ;    - copy the comments from the source file into the header.

--- digital/io/src/path.h
+++ digital/io/src/path.h
@@ -25,18 +25,28 @@
  *
  * }}} */
 
+/** Initialise path finder. */
 void
 path_init (void);
 
+/** Setup end points (source and destination coordinates). */
 void
 path_endpoints (int16_t sx, int16_t sy, int16_t dx, int16_t dy);
 
+/** Set up an obstacle at given position with the given radius and validity
+ * period. */
 void
 path_obstacle (uint8_t i, int16_t x, int16_t y, uint16_t r, uint16_t valid);
 
+/** Slowly make the obstacles disappear. */
+void
+path_decay (void);
+
+/** Compute shortest path. */
 void
 path_update (void);
 
+/** Retrieve first path point coordinates.  Return 0 on failure. */
 uint8_t
 path_get_next (uint16_t *x, uint16_t *y);
 
