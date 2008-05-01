d2fa639892a31069475e1303c42fc6ef2ecc650c
Nicolas Schodet <nicolas.schodet@apbteam.org>
2008-05-01 04:47:28 +0200

    * digital/asserv/src/asserv:    - updated speed parameters.

--- digital/asserv/tools/init.py
+++ digital/asserv/tools/init.py
@@ -4,8 +4,8 @@ host = dict (
 	tkp = 1, tkd = 16, akp = 2, akd = 16,
 	a0kp = 1, a0kd = 16,
 	E = 0x3ff, D = 0x1ff,
-	ta = 0.5, aa = 0.5, tsm = 0x60, tss = 0x40, asm = 0x60, ass = 0x40,
-	a0a = 0.5, a0sm = 0x10, a0ss = 0x05,
+	ta = 0.5, aa = 0.5, tsm = 0x60, tss = 0x20, asm = 0x30, ass = 0x10,
+	a0a = 0.5, a0sm = 0x0a, a0ss = 0x05,
 	)
 target = dict (
 	scale = 0.0413530725332892, f = 0xcfa,
@@ -13,6 +13,6 @@ target = dict (
 	tkp = 1, tkd = 16, akp = 2, akd = 16,
 	a0kp = 0.8, a0ki = 0.05, a0kd = 0.05,
 	E = 0x3ff, D = 0x1ff, I = 0x1fff,
-	ta = 0.5, aa = 0.5, tsm = 0x60, tss = 0x40, asm = 0x60, ass = 0x40,
-	a0a = 0.5, a0sm = 0x10, a0ss = 0x05,
+	ta = 0.5, aa = 0.5, tsm = 0x60, tss = 0x20, asm = 0x30, ass = 0x10,
+	a0a = 0.5, a0sm = 0x0a, a0ss = 0x05,
 	)
