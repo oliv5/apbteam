d8d05fbbbd925d37fe846be91715d57ef5c19d21
Jérémy Dufour <jeremy.dufour@apbteam.org>
2008-05-10 00:47:12 +0200

    * digital/io/src    - increase sharp filter value.

--- digital/io/src/sharp.c
+++ digital/io/src/sharp.c
@@ -52,7 +52,7 @@ uint8_t sharp_cache_interpreted_[SHARP_NUMBER];
 /**
  * Filter number value before changing state.
  */
-#define SHARP_FILTER_NUMBER 5
+#define SHARP_FILTER_NUMBER 6
 
 /**
  * Previous sharp interpreted values.
