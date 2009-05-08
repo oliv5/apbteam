7aaed3808cd30db69c63af3c6554debc867dda93
Jérémy Dufour <jeremy.dufour@apbteam.org>
2009-05-09 01:55:00 +0200

    * digital/io:    - add contact module.

---
+++ digital/io/src/contact.h
@@ -0,0 +1,42 @@
+#ifndef contact_h
+#define contact_h
+/* contact.h */
+/* io - Input & Output with Artificial Intelligence (ai) support on AVR. {{{
+ *
+ * Copyright (C) 2009 Dufour Jérémy
+ *
+ * APBTeam:
+ *        Web: http://apbteam.org/
+ *      Email: team AT apbteam DOT org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * }}} */
+
+#define CONTACT_FILTER_BRIDGE_PUCK C, 0
+#define CONTACT_ELEVATOR_DOOR C, 1
+
+/**
+ * Initialize the contact module.
+ */
+void
+contact_init (void);
+
+/**
+ * To get a state of a contact, just do:
+ * IO_GET (CONTACT_FILTER_BRIDGE_PUCK);
+ */
+
+#endif /* contact_h */
