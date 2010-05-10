7529c4d986717ac51058a2354b53afc81272a028
Nicolas Schodet <nicolas.schodet@apbteam.org>
2010-05-10 23:54:42 +0200

    digital/asserv/src/asserv: update contacts to match hardware

--- digital/asserv/src/asserv/contacts.h
+++ digital/asserv/src/asserv/contacts.h
@@ -31,7 +31,7 @@
 #define CONTACT_FRONT_LEFT_IO C, 2
 #define CONTACT_FRONT_RIGHT_IO C, 3
 #define CONTACT_CENTER_IO C, 4
-#define CONTACT_AUX0_ZERO_IO C, 5
-#define CONTACT_AUX1_ZERO_IO C, 7
+#define CONTACT_AUX0_ZERO_IO C, 7
+#define CONTACT_AUX1_ZERO_IO C, 5
 
 #endif /* contacts_h */
