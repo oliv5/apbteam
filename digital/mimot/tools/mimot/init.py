22ac49f3a2481e729903db3772f53741b9c19f3c
Nicolas Schodet <nicolas.schodet@apbteam.org>
2010-05-29 12:27:13 +0200

    digital/mimot/tools: change blocking detection parameters

--- digital/mimot/tools/mimot/init.py
+++ digital/mimot/tools/mimot/init.py
@@ -2,19 +2,19 @@
 host = dict (
         a0kp = 4,
         a0a = 16, a0sm = 0x60, a0ss = 0x10,
-        a0be = 256, a0bs = 0x04, a0bc = 20,
+        a0be = 256, a0bs = 0x18, a0bc = 5,
         a1kp = 4,
         a1a = 16, a1sm = 0x60, a1ss = 0x10,
-        a1be = 256, a1bs = 0x04, a1bc = 20,
+        a1be = 256, a1bs = 0x18, a1bc = 5,
         E = 0x3ff, D = 0x1ff,
         )
 target = dict (
         a0kp = 4,
         a0a = 16, a0sm = 0x60, a0ss = 0x10,
-        a0be = 256, a0bs = 0x04, a0bc = 20,
+        a0be = 256, a0bs = 0x18, a0bc = 5,
         a1kp = 4,
         a1a = 16, a1sm = 0x60, a1ss = 0x10,
-        a1be = 256, a1bs = 0x04, a1bc = 20,
+        a1be = 256, a1bs = 0x18, a1bc = 5,
         E = 0x3ff, D = 0x1ff,
         w = 0x03,
         )
