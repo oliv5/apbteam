74e86e39840c2d83811b01ddc69acecde2b104ab
Nicolas Schodet <nicolas.schodet@apbteam.org>
2010-11-29 23:07:58 +0100

    digital/dev2/src/usb_gpio: remove unused uart configuration

--- digital/dev2/src/usb_gpio/avrconfig.h
+++ digital/dev2/src/usb_gpio/avrconfig.h
@@ -30,49 +30,6 @@
  * 8000000, 11059200, 14745600, 16000000, 18432000, 20000000. */
 #define AC_FREQ 8000000
 
-/* uart - UART module. */
-/** Select hardware uart for primary uart: 0, 1 or -1 to disable. */
-#define AC_UART0_PORT 1
-/** Baudrate: 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 76800,
- * 115200, 230400, 250000, 500000, 1000000. */
-#define AC_UART0_BAUDRATE 38400
-/** Send mode:
- *  - POLLING: no interrupts.
- *  - RING: interrupts, ring buffer. */
-#define AC_UART0_SEND_MODE RING
-/** Recv mode, same as send mode. */
-#define AC_UART0_RECV_MODE RING
-/** Character size: 5, 6, 7, 8, 9 (only 8 implemented). */
-#define AC_UART0_CHAR_SIZE 8
-/** Parity : ODD, EVEN, NONE. */
-#define AC_UART0_PARITY EVEN
-/** Stop bits : 1, 2. */
-#define AC_UART0_STOP_BITS 1
-/** Send buffer size, should be power of 2 for RING mode. */
-#define AC_UART0_SEND_BUFFER_SIZE 16
-/** Recv buffer size, should be power of 2 for RING mode. */
-#define AC_UART0_RECV_BUFFER_SIZE 32
-/** If the send buffer is full when putc:
- *  - DROP: drop the new byte.
- *  - WAIT: wait until there is room in the send buffer. */
-#define AC_UART0_SEND_BUFFER_FULL WAIT
-/** In HOST compilation:
- *  - STDIO: use stdin/out.
- *  - PTS: use pseudo terminal. */
-#define AC_UART0_HOST_DRIVER STDIO
-/** Same thing for secondary port. */
-#define AC_UART1_PORT -1
-#define AC_UART1_BAUDRATE 115200
-#define AC_UART1_SEND_MODE RING
-#define AC_UART1_RECV_MODE RING
-#define AC_UART1_CHAR_SIZE 8
-#define AC_UART1_PARITY EVEN
-#define AC_UART1_STOP_BITS 1
-#define AC_UART1_SEND_BUFFER_SIZE 32
-#define AC_UART1_RECV_BUFFER_SIZE 32
-#define AC_UART1_SEND_BUFFER_FULL WAIT
-#define AC_UART1_HOST_DRIVER PTS
-
 /* usb */
 #include "modules/usb/lufaconfig.h"
 
