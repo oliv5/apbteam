7ddda7dc89ec24984c6569e54004654eb3aa9b79
Nicolas Schodet <nicolas.schodet@apbteam.org>
2011-05-28 12:34:37 +0200

    digital/mimot: new blocking parameters

--- digital/mimot/tools/mimot/init.py
+++ digital/mimot/tools/mimot/init.py
@@ -12,10 +12,10 @@ target_marcel = dict (
 target_robospierre = dict (
         a0kp = 8, a0kd = 1,
         a0a = 8, a0sm = 0x60, a0ss = 0x10,
-        a0be = 256, a0bs = 0x18, a0bc = 5,
+        a0be = 32, a0bs = 0x08, a0bc = 125,
         a1kp = 4,
         a1a = 0.5, a1sm = 0x30, a1ss = 0x08,
-        a1be = 256, a1bs = 0x18, a1bc = 5,
+        a1be = 64, a1bs = 0x08, a1bc = 5,
         E = 0x3ff, D = 0x1ff,
         w = 0x03,
         )
