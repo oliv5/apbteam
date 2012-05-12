669557d59809da7eb74f2437183941be3713f3c6
Julien Balleyguier <julien@enoch.(none)>
2012-05-12 12:27:39 +0200

    digital/mimot/tools/mimot/init.py: typo correction and new values for guybrush

--- digital/mimot/tools/mimot/init.py
+++ digital/mimot/tools/mimot/init.py
@@ -2,25 +2,25 @@
 target_marcel = dict (
         a0_kp = 4,
         a0_acc = 16, a0_speed_max = 0x60, a0_speed_slow = 0x10,
-        a0_error_limit = 256, a0_bd_speed_limit = 0x18, a0_bd_counter_limit = 5,
+        a0_bd_error_limit = 256, a0_bd_speed_limit = 0x18, a0_bd_counter_limit = 5,
         a1_kp = 4,
         a1_acc = 16, a1_speed_max = 0x60, a1_speed_slow = 0x10,
-        a1_error_limit = 256, a1_bd_speed_limit = 0x18, a1_bd_counter_limit = 5,
+        a1_bd_error_limit = 256, a1_bd_speed_limit = 0x18, a1_bd_counter_limit = 5,
         e_sat = 0x3ff, d_sat = 0x1ff,
         )
 target_robospierre = dict (
         a0_kp = 8, a0_kd = 1,
         a0_acc = 2, a0_speed_max = 0x60, a0_speed_slow = 0x10,
-        a0_error_limit = 32, a0_bd_speed_limit = 0x08, a0_bd_counter_limit = 125,
+        a0_bd_error_limit = 32, a0_bd_speed_limit = 0x08, a0_bd_counter_limit = 125,
         a1_kp = 4,
         a1_acc = 0.5, a1_speed_max = 0x30, a1_speed_slow = 0x08,
-        a1_error_limit = 64, a1_bd_speed_limit = 0x08, a1_bd_counter_limit = 5,
+        a1_bd_error_limit = 64, a1_bd_speed_limit = 0x08, a1_bd_counter_limit = 5,
         e_sat = 0x3ff, d_sat = 0x1ff,
         )
 target_guybrush = dict (
         a0_kp = 12, a0_kd = 32,
-        a0_acc = 1, a0_speed_max = 0x20, a0_speed_slow = 0x02,
-        a0_error_limit = 32, a0_bd_speed_limit = 0x08, a0_bd_counter_limit = 125,
+        a0_acc = 2, a0_speed_max = 0x20, a0_speed_slow = 0x02,
+        a0_bd_error_limit = 22, a0_bd_speed_limit = 0x08, a0_bd_counter_limit = 50,
         e_sat = 0x3ff, d_sat = 0x1ff,
         )
 target = {
