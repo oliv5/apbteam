7fa64b61e5f10ee48e246b4dd6051b4924c6abbf
Nicolas Schodet <nicolas.schodet@apbteam.org>
2012-05-19 21:43:07 +0200

    digital/ai/src/utils/chrono: add chrono margin again

--- digital/ai/src/utils/chrono.h
+++ digital/ai/src/utils/chrono.h
@@ -30,7 +30,7 @@
  */
 
 /** Duration of a match in milliseconds, with margin. */
-#define CHRONO_MATCH_DURATION_MS (90000 - 3500)
+#define CHRONO_MATCH_DURATION_MS (90000 - 5500)
 
 /** Start chrono count down. */
 void
